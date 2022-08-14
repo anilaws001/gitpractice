5d8bbd480e92b82a55f0353e9f31056b9c4b5539
root <root@DESKTOP-I1A9I25.localdomain>
2022-08-14 18:58:09 +0300

    first commit
    	 Committer: Anil <Anil@DESKTOP-I1A9I25.localdomain>

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello
+{
+	Public static void main(String args[])
+	{
+		System,out.printIn("hello");
+		{
+		}class Hello
+		{
+			Public static void main(String args[])
+			{
+				System,out.printIn("hello");
+				{
+				}
